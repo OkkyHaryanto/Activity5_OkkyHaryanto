2befab78703622fe9c89626f4844412bc59af814
OkkyHaryanto <63486943+OkkyHaryanto@users.noreply.github.com>
2021-03-21 13:43:26 +0700

    Acivity2

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sampleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -30,29 +32,48 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 nama = edemail.getText().toString();
                 password = edpassword.getText().toString();
 
-                if(edemail.getText().toString().equals("okky@mail.com")&&edpassword.getText().toString().equals("okky123")) {
+                String email = "okky@mail.com";
+                String pass = "123";
+
+                if (nama.isEmpty() || password.isEmpty()) {
+
                     Toast t = Toast.makeText(getApplicationContext(),
-                            "LOGIN SUKSES || email : " + nama + " password : " + password + "", Toast.LENGTH_LONG);
+                            "Email dan password wajib diisi!!!",
+                            Toast.LENGTH_LONG);
                     t.show();
-                }
-                else if (!edemail.getText().toString().equals("okky@mail.com")&&edpassword.getText().toString().equals("okky123")) {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            " Email anda salah ", Toast.LENGTH_SHORT);
-                    t.show();
-                }
-                else if (!edpassword.getText().toString().equals("okky123")&&edemail.getText().toString().equals("okky@mail.com")) {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            " Password anda salah ", Toast.LENGTH_SHORT);
-                    t.show();
-                }
-                else{
-                    Toast.makeText((getApplicationContext()), "Email atau Password Anda salah",
-                            Toast.LENGTH_SHORT).show();
-                }
+                } else {
+                    if (nama.equals(email) && password.equals(pass)) {
+
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Sukses",
+                                Toast.LENGTH_LONG);
+
+                        t.show();
+
+                        Bundle b = new Bundle();
+
+                        b.putString("a", nama.trim());
+
+                        b.putString("b", password.trim());
+
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                        i.putExtras(b);
+
+                        startActivity(i);
+
+
+                    } else {
+
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Gagal", Toast.LENGTH_LONG);
+
+                        t.show();
+            }     }
             }
         });
     }
